6d3147df4854ba632bd45e56a5c0a80cb9fc66da
connections-notice <idud@ken.com>
2019-07-31 01:34:48 -0700

    sec

---
+++ karyi/roll.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $("#showPanel").click(function(){
+        $("#panel").fadeIn("slow");
+    });
+    
+    $("#hidePanel").click(function(){
+        $("#panel").fadeOut("slow");
+    });
+  
+});
